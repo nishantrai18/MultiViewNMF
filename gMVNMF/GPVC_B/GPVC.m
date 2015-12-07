49e6e40d37a11550a03cd91d2d27c6032b9a1450
Nishant Rai <nishantr@iitk.ac.in>
2015-12-07 16:11:24 +0530

    Completed skeleton for Partial GNMF_A

--- gMVNMF/GPVC_B/GPVC.m
+++ gMVNMF/GPVC_B/GPVC.m
@@ -1,146 +1,110 @@
-function [finalU, finalV, finalcentroidV, log] = GMultiNMF(X, K, W, label,options)
-%	Notation:
-% 	X ... a cell containing all views for the data
-% 	K ... number of hidden factors
-% 	W ... weight matrix of the affinity graph 
-% 	label ... ground truth labels
-
+function [Ux,Uy,centroidPc,P1,P2,objValue] = GPVC(X2,Y2,X1,Y3,W1,W2,options)
+%      partial view multi-view data set :   X2: n2*dx  Y2: n2*dy  X1: n1*dx  Y3: n3*dy
+%      (X2,Y2); examples appearing in both views; 
+%      (X1,);   examples appearing in only view x
+%      (,Y3):   examples appearing in only view y  
+%      W1 : Weight matrix for the affinity graph of View 1
+%      W2 : Weight matrix for the affinity graph of View 2
+%      dx,dy:   feature dimension of viewx viewy
+%      option.alpha: parameter controls the importance of views
+%      option.latentdim: the feature dimension of latent space(default value: cluster number)
+%   ouptut: 
+%     Ux, Uy:   (k*dx, k*dy)  basis of latent space
+%     P2,P1,P3: (n2*k, n1*k, n3*k)  data representations for examples in the latent space 
+%     centroidP2 : Refers to the consensus matrix for the complete datapoints
 %	Writen by Jialu Liu (jliu64@illinois.edu)
 % 	Modified by Zhenfan Wang (zfwang@mail.dlut.edu.cn)
-
-%	References:
-% 	J. Liu, C.Wang, J. Gao, and J. Han, ��Multi-view clustering via joint nonnegative matrix factorization,�� in Proc. SDM, Austin, Texas, May 2013, vol. 13, pp. 252�C260.
-% 	Zhenfan Wang, Xiangwei Kong, Haiyan Fu, Ming Li, Yujia Zhang, FEATURE EXTRACTION VIA MULTI-VIEW NON-NEGATIVE MATRIX FACTORIZATION WITH LOCAL GRAPH REGULARIZATION, ICIP 2015.
+%   Further modified by Nishant Rai (nishantr AT iitk DOT ac DOT in)
 
 %Note that columns are data vectors here
+%Number of views are assumed to be 2 in this code
 
 tic;
-viewNum = length(X);
 Rounds = options.rounds;
-nSmp=size(X{1},2);                          %Number of data points
-U_ = [];
-V_ = [];
+alpha=options.alpha;
+beta=options.beta;
+error=options.error;
 
-U = cell(1, viewNum);
-V = cell(1, viewNum);
+K = options.K;
+A1 = horzcat(X1,X2);
+A2 = horzcat(Y2,Y3);
 
-j = 2;
-log = 0;
-ac=0;
+nSmp = size(W1,1);
+Wtemp = beta*alpha*W1;            %Modify the weight matrix with the involved parameters
+DCol = full(sum(Wtemp,2));
+D = spdiags(DCol,0,nSmp,nSmp);
+L1 = D - Wtemp;                              %Get matrix L
 
-%% initialize basis and coefficient matrices, initialize on the basis of
-% standard GNMF algorithm
+nSmp = size(W2,1);
+Wtemp = beta*alpha*W1;            %Modify the weight matrix with the involved parameters
+DCol = full(sum(Wtemp,2));
+D = spdiags(DCol,0,nSmp,nSmp);
+L2 = D - Wtemp;                              %Get matrix L
+    
+[numCom,~]=size(X2');
+
+objValue = [];
+
+%% initialize basis and coefficient matrices, initialize on the basis of standard GNMF algorithm
 tic;
-while j < 3                   %Modify this loop for managing initializations
-                               %Analyse which 'options' give better results
-    j = j + 1;
-    workspace
-    Goptions.alpha=options.Gaplpha;
-    if j == 1
-        
-        %[U{1}, V{1}] = NMF1(X{1}, K,  options, U_, V_);
-        rand('twister',5489);
-        [U{1}, V{1}] = GNMF(X{1}, K, W{1}, Goptions);                       %In this case, random inits take place
-        rand('twister',5489);
-        printResult(V{1}, label, options.K, options.kmeans);
-    else
-        %[U{1}, V{1}] = NMF1(X{1}, K, options, U_, V{viewNum});
-        rand('twister',5489);
-        [U{1}, V{1}] = GNMF(X{1}, K, W{1}, options);        %In this case, random inits take place
-        rand('twister',5489);
-        printResult(V{1}, label, options.K, options.kmeans);        
-    end
-    for i = 2:viewNum
-    %         [U{i}, V{i}] = NMF1(X{i}, K, options, U_, V{i-1});
-        rand('twister',5489);
-        [U{i}, V{i}] = GNMF(X{i}, K, W{i}, Goptions);
-        rand('twister',5489);
-        printResult(V{i}, label, options.K, options.kmeans);
-    end
-end
+j = j + 1;
+Goptions.alpha=options.Gaplpha;
+rand('twister',5489);
+[Ux, P1] = GNMF(A1, K, W1, options);        %In this case, random inits take place
+rand('twister',5489);
+printResult(P1, label, options.K, options.kmeans);        
+rand('twister',5489);
+[Uy, P2] = GNMF(A2, K, W2, Goptions);
+rand('twister',5489);
+printResult(P2, label, options.K, options.kmeans);
+
 toc;
 %%
 
-%%Alternate Optimisations for consensus matrix and individual view matrices
-optionsForPerViewNMF = options;
-oldac=0;                        %Old accuracy
-maxac=0;                        %Maximum accuracy
+%% Alternate Optimisations for consensus matrix and individual view matrices
+optionsPGNMF = options;
 j = 0;
 sumRound=0;
 while j < Rounds                            %Number of rounds of AO
     sumRound=sumRound+1;
     j = j + 1;
     if j==1
-        centroidV = V{1};                       %Basic initialization for consensus matrix
+        centroidPc = P1(1:numCom,:);                       %Basic initialization for consensus matrix
     else
-        centroidV = options.alphas(1) * V{1};           %From the paper, we have a definite solution for V*
-        for i = 2:viewNum                               %CHECK why have we not considered Q in the normalization
-                                                        %Already normalized during the optimizations
-            centroidV = centroidV + options.alphas(i) * V{i};
-        end
-        centroidV = centroidV / sum(options.alphas);
+        centroidPc = (alpha*P1(1:numCom,:)) + (alpha*P2((numCom+1):end,:));           %From the paper, we have a definite solution for V*
+        centroidPc = centroidPc / sum(options.alphas);
     end
     logL = 0;                                   %Loss for the round
-    for i = 1:viewNum
-        alpha=options.alphas(i);
-        if alpha > 0
-            Wtemp = options.beta*alpha*W{i};            %Modify the weight matrix with the involved parameters
-            DCol = full(sum(Wtemp,2));
-            D = spdiags(DCol,0,nSmp,nSmp);
-            L = D - Wtemp;                              %Get matrix L
-            if isfield(options,'NormW') && options.NormW
-                D_mhalf = spdiags(DCol.^-.5,0,nSmp,nSmp) ;
-                L = D_mhalf*L*D_mhalf;
-            end
-        else
-            L = [];
-        end
-        %Compute the losses
-        tmp1 = (X{i} - U{i}*V{i}');
-        tmp2 = (V{i} - centroidV);
-        logL = logL + sum(sum(tmp1.^2)) + alpha* (sum(sum(tmp2.^2)))+sum(sum((V{i}'*L).*V{i}'));  %�޸ģ�����SampleW��V'*L*V
-    end
+    
+    %Compute the losses
+    tmp1 = (A1 - Ux*P1');
+    tmp2 = (P1 - centroidPc);
+    logL = logL + sum(sum(tmp1.^2)) + alpha* (sum(sum(tmp2.^2)))+sum(sum((P1'*L1).*P1'));
+    tmp1 = (A2 - Uy*P2');
+    tmp2 = (P2 - centroidPc);
+    logL = logL + sum(sum(tmp1.^2)) + alpha* (sum(sum(tmp2.^2)))+sum(sum((P2'*L2).*P2'));
     
     %logL
-    log(end+1)=logL;                %End indicates last index of array, so basically push operation
-    rand('twister',5489);
-    ac = printResult(centroidV, label, options.K, options.kmeans);
+    objValue = [objValue logL];                %End indicates last index of array, so basically push operation
     
-    if ac>oldac
-        tempac=ac;
-        tempU=U;
-        tempV=V;
-        tempcentroidV=centroidV;
-    elseif oldac>maxac
-        maxac=oldac;
-        maxU=tempU;
-        maxV=tempV;
-        maxcentroidV=tempcentroidV;
+    if mod(j,10)==0
+    fprintf('Iteration %d, objective value %g\n', j, objValue(j));
     end
-    
-    %Find the Best combination for U, V based on accuracy
-    oldac=ac;
-    if(tempac>maxac)
-        finalU=tempU;
-        finalV=tempV;
-        finalcentroidV=tempcentroidV;
 
-    else
-        finalU=maxU;
-        finalV=maxV;
-        finalcentroidV=maxcentroidV;
+    if j>1 && ((abs(objValue(j)-objValue(j-1))/objValue(j) < error)|| objValue(j)<=error)
+        fprintf('Objective value converge to %g at iteration %d before the maxIteration reached \n',objValue(j),j);
+        break;
     end
-    
+
     if sumRound==30
         break;
     end
     
-    for i = 1:viewNum
-        optionsForPerViewNMF.alpha = options.alphas(i);
-        rand('twister',5489);
-        [U{i}, V{i}] = PerViewNMF(X{i}, K, centroidV, W{i} , optionsForPerViewNMF, finalU{i}, finalV{i});
-        %Peform optimization with V* (centroidV) fixed and inits finalU, finalV
-    end
+    optionsPGNMF.alpha = alpha;
+    rand('twister',5489);
+    [Ux, P1] = PartialGNMF(A1, K, centroidPc, W1, optionsPGNMF, Ux, P1);
+    [Uy, P2] = PartialGNMF(A2, K, centroidPc, W2, optionsPGNMF, Uy, P2);
+    %Peform optimization with Pc* (centroidPc) fixed and inits finalU, finalV
 
 end
 toc
