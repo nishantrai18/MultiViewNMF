8ff6f7b753f6a9888b663d1c8b42572016a90171
Nishant Rai <nishantr@iitk.ac.in>
2015-12-12 18:08:02 +0530

    Added code for creating mat(data) files for images formed using the py script

---
+++ partialMV/PVC/recreateResults/formatData/formatImage.m
@@ -0,0 +1,24 @@
+clear all;
+%clc;
+datasetdir='../data/';
+dataname={'orl'};
+
+for idata=1:length(dataname)  
+    load 'orlHog.mat'
+    X1 = feat.fea;
+    X2 = feat.hogs;
+    truth = feat.gnd;
+    dataf=strcat(datasetdir,lower(dataname(idata)),'RnSp.mat');        %Just the datafile name
+    save(cell2mat(dataf(1)), 'X1','X2', 'truth');
+
+    folds = [];
+    numInst = size(truth);
+    numInst = numInst(1);
+    numPerms = 30;
+    for j=1:numPerms
+        folds = [folds;randperm(numInst)];
+    end
+    
+    dataf=strcat(datasetdir,lower(dataname(idata)),'Folds.mat');        %Just the datafile name
+    save(cell2mat(dataf(1)), 'folds');
+end    
