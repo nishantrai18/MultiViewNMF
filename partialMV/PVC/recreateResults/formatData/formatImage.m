f865c185c6bf23a4f067a560c1e9ff973256b238
Nishant Rai <nishantr@iitk.ac.in>
2015-12-12 18:59:56 +0530

    Modified it too remove any negative values (i.e. set them to 0)

--- partialMV/PVC/recreateResults/formatData/formatImage.m
+++ partialMV/PVC/recreateResults/formatData/formatImage.m
@@ -7,6 +7,8 @@ for idata=1:length(dataname)
     load 'orlHog.mat'
     X1 = feat.fea;
     X2 = feat.hogs;
+    X1(X1<0) = 0;
+    X2(X2<0) = 0;
     truth = feat.gnd;
     dataf=strcat(datasetdir,lower(dataname(idata)),'RnSp.mat');        %Just the datafile name
     save(cell2mat(dataf(1)), 'X1','X2', 'truth');
